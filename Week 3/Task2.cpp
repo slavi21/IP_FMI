42039a4d19246d454b88f5927fe8d8ae59b470ba
Slaveya Antonova <92144433+slavi21@users.noreply.github.com>
2024-08-14 20:38:05 +0300

    Update Task2.cpp

--- Week 3/Task2.cpp
+++ Week 3/Task2.cpp
@@ -1,7 +1,6 @@
 // Task2: Write a program that takes two natural numbers and returns their greatest common divisor.
 
 #include <iostream>
-using namespace std;
 
 // Euclidean algorithm
 int gcd(int a, int b) {
@@ -15,10 +14,10 @@ int gcd(int a, int b) {
 
 int main() {
     int n, k;
-    cout << "Enter two numbers: ";
-    cin >> n >> k;
+    std::cout << "Enter two numbers: ";
+    std::cin >> n >> k;
 
-    cout << "Greatest Common Divisor of " << n << " and " << k << " is: " << gcd(n, k) << endl;
+    std::cout << "Greatest Common Divisor of " << n << " and " << k << " is: " << gcd(n, k) << endl;
 
     return 0;
 }
